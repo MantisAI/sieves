8f87f4d806b2973320b2aa45f7a2230ecaad5a65
Raphael Mitsch <raphael.mitsch.ct@datamaran.com>
2024-12-29 16:57:53 +0100

    Fix workflow file. Separate tests.

--- sieves/tests/test_pipeline.py
+++ sieves/tests/test_pipeline.py
@@ -8,21 +8,15 @@ from tokenizers import tokenizers
 from sieves import Doc, Pipeline, engines, tasks
 
 
-def test_pipeline() -> None:
+def test_pipeline_outlines() -> None:
     outlines_model_name = "HuggingFaceTB/SmolLM-135M-Instruct"
     engine_outlines = engines.outlines_engine.Outlines(model=outlines.models.transformers(outlines_model_name))
-    engine_dspy = engines.dspy_engine.DSPy(
-        model=dspy.LM("claude-3-haiku-20240307", api_key=os.environ["ANTHROPIC_API_KEY"])
-    )
 
     all_tasks = [
         tasks.parsers.Docling(),
-        tasks.chunkers.Chonkie(chonkie.TokenChunker(tokenizers.Tokenizer.from_pretrained(outlines_model_name))),
+        tasks.chunkers.Chonkie(chonkie.TokenChunker(tokenizers.Tokenizer.from_pretrained("gpt2"))),
         tasks.predictive.Classification(
             task_id="classifier_outlines", labels=["scientific paper", "newspaper article"], engine=engine_outlines
-        ),
-        tasks.predictive.Classification(
-            task_id="classifier_dspy", labels=["scientific paper", "newspaper article"], engine=engine_dspy
         ),
     ]
 
@@ -37,5 +31,25 @@ def test_pipeline() -> None:
     assert "classifier_dspy" in docs[0].results
 
 
-if __name__ == "__main__":
-    test_pipeline()
+def test_pipeline_dspy() -> None:
+    engine_dspy = engines.dspy_engine.DSPy(
+        model=dspy.LM("claude-3-haiku-20240307", api_key=os.environ["ANTHROPIC_API_KEY"])
+    )
+
+    all_tasks = [
+        tasks.parsers.Docling(),
+        tasks.chunkers.Chonkie(chonkie.TokenChunker(tokenizers.Tokenizer.from_pretrained("gpt2"))),
+        tasks.predictive.Classification(
+            task_id="classifier_dspy", labels=["scientific paper", "newspaper article"], engine=engine_dspy
+        ),
+    ]
+
+    resources = [Doc(uri="https://arxiv.org/pdf/2408.09869")]
+    pipe = Pipeline(all_tasks)
+    docs = list(pipe(resources))
+
+    assert len(docs) == 1
+    assert docs[0].text
+    assert docs[0].chunks
+    assert "classifier_outlines" in docs[0].results
+    assert "classifier_dspy" in docs[0].results
