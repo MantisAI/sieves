fd073c5994e8fb5cd8bbe242d5433cc85cbb4c11
Raphael Mitsch <raphael.mitsch.ct@datamaran.com>
2024-12-29 21:14:56 +0100

    Readd dummy_docs import.

--- sieves/__init__.py
+++ sieves/__init__.py
@@ -6,8 +6,6 @@ from .pipeline import Pipeline
 __all__ = ["Doc", "tasks", "Pipeline"]
 
 # todo
-#  - Add progress bar support in PredictiveTask (should work with Iterable returned by engines)
-#  - Add more engines (GliX (not generative!), JsonFormer)
 #  - Add few-shot example support
 #    - https://dspy.ai/deep-dive/data-handling/examples/
 #  - Chunking support (on task-level)
