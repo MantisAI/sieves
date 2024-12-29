a3ddb7d55e2b31997ff39de84217bf6c5c152d02
Raphael Mitsch <raphael.mitsch.ct@datamaran.com>
2024-12-29 21:29:33 +0100

    Remove todos (now in repo as issues).

--- sieves/__init__.py
+++ sieves/__init__.py
@@ -4,18 +4,3 @@ from sieves.data import Doc
 from .pipeline import Pipeline
 
 __all__ = ["Doc", "tasks", "Pipeline"]
-
-# todo
-#  - Add few-shot example support
-#    - https://dspy.ai/deep-dive/data-handling/examples/
-#  - Chunking support (on task-level)
-#  - Allow for customization of prompt templates
-#  - Allow engine-specific settings in engine constructors
-#  - Serialization
-#  - Tasks
-#    - Classification: add multi-class support
-#    - NER
-#    - Entity linking
-#    - KG construction
-#    - Translation
-#    ...
