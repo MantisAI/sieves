05efe0f8d40673eb35c856612200c3e4d7de4725
Raphael Mitsch <raphael.mitsch.ct@datamaran.com>
2024-12-29 16:57:53 +0100

    Fix workflow file. Separate tests.

--- sieves/tests/test_pipeline.py
+++ sieves/tests/test_pipeline.py
@@ -1,41 +1,59 @@
+# mypy: ignore-errors
 import os
 
 import chonkie
 import dspy
 import outlines
+import pytest
 from tokenizers import tokenizers
 
 from sieves import Doc, Pipeline, engines, tasks
 
 
-def test_pipeline() -> None:
-    outlines_model_name = "HuggingFaceTB/SmolLM-135M-Instruct"
-    engine_outlines = engines.outlines_engine.Outlines(model=outlines.models.transformers(outlines_model_name))
-    engine_dspy = engines.dspy_engine.DSPy(
-        model=dspy.LM("claude-3-haiku-20240307", api_key=os.environ["ANTHROPIC_API_KEY"])
-    )
-
-    all_tasks = [
-        tasks.parsers.Docling(),
-        tasks.chunkers.Chonkie(chonkie.TokenChunker(tokenizers.Tokenizer.from_pretrained(outlines_model_name))),
-        tasks.predictive.Classification(
-            task_id="classifier_outlines", labels=["scientific paper", "newspaper article"], engine=engine_outlines
-        ),
-        tasks.predictive.Classification(
-            task_id="classifier_dspy", labels=["scientific paper", "newspaper article"], engine=engine_dspy
-        ),
-    ]
-
+@pytest.fixture(scope="session")
+def preprocessed_docs() -> list[Doc]:
     resources = [Doc(uri="https://arxiv.org/pdf/2408.09869")]
-    pipe = Pipeline(all_tasks)
-    docs = list(pipe(resources))
+    pipe = Pipeline(
+        tasks=[
+            tasks.parsers.Docling(),
+            tasks.chunkers.Chonkie(chonkie.TokenChunker(tokenizers.Tokenizer.from_pretrained("gpt2"))),
+        ]
+    )
+    return list(pipe(resources))
+
+
+def test_pipeline_outlines(preprocessed_docs) -> None:
+    outlines_model_name = "gpt2"
+    engine_outlines = engines.outlines_engine.Outlines(model=outlines.models.transformers(outlines_model_name))
+    pipe = Pipeline(
+        [
+            tasks.predictive.Classification(
+                task_id="classifier_outlines", labels=["scientific paper", "newspaper article"], engine=engine_outlines
+            ),
+        ]
+    )
+    docs = list(pipe(preprocessed_docs))
 
     assert len(docs) == 1
     assert docs[0].text
     assert docs[0].chunks
     assert "classifier_outlines" in docs[0].results
+
+
+def test_pipeline_dspy(preprocessed_docs) -> None:
+    engine_dspy = engines.dspy_engine.DSPy(
+        model=dspy.LM("claude-3-haiku-20240307", api_key=os.environ["ANTHROPIC_API_KEY"])
+    )
+    pipe = Pipeline(
+        [
+            tasks.predictive.Classification(
+                task_id="classifier_dspy", labels=["scientific paper", "newspaper article"], engine=engine_dspy
+            ),
+        ]
+    )
+    docs = list(pipe(preprocessed_docs))
+
+    assert len(docs) == 1
+    assert docs[0].text
+    assert docs[0].chunks
     assert "classifier_dspy" in docs[0].results
-
-
-if __name__ == "__main__":
-    test_pipeline()
