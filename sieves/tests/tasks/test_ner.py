d52eb507ebaecfd5c6d4d6dd6bbac2e29f710046
adgianv <a.digianvito@hotmail.it>
2025-04-02 13:44:54 +0200

    added serialization test

--- sieves/tests/tasks/test_ner.py
+++ sieves/tests/tasks/test_ner.py
@@ -3,6 +3,7 @@ import pytest
 
 from sieves import Pipeline
 from sieves.engines import EngineType
+from sieves.serialization import Config
 from sieves.tasks.predictive import ner
 from sieves.tasks.predictive.ner.core import Entity
 
@@ -51,3 +52,45 @@ def test_run(ner_docs, batch_engine, fewshot) -> None:
     assert len(docs) == 2
     for doc in docs:
         assert "NER" in doc.results
+
+
+@pytest.mark.parametrize("batch_engine", [EngineType.ollama], indirect=["batch_engine"])
+def test_serialization(ner_docs, batch_engine) -> None:
+    pipe = Pipeline([ner.NER(entities=["PERSON", "LOCATION", "COMPANY"], engine=batch_engine)])
+    list(pipe(ner_docs))
+
+    config = pipe.serialize()
+    assert config.model_dump() == {
+        "cls_name": "sieves.pipeline.core.Pipeline",
+        "tasks": {
+            "is_placeholder": False,
+            "value": [
+                {
+                    "cls_name": "sieves.tasks.predictive.ner.core.NER",
+                    "engine": {
+                        "is_placeholder": False,
+                        "value": {
+                            "cls_name": "sieves.engines.wrapper.Engine",
+                            "inference_kwargs": {"is_placeholder": False, "value": {}},
+                            "init_kwargs": {"is_placeholder": False, "value": {}},
+                            "model": {"is_placeholder": True, "value": "sieves.engines.ollama_.Model"},
+                            "version": Config.get_version(),
+                        },
+                    },
+                    "entities": {"is_placeholder": False, "value": ["PERSON", "LOCATION", "COMPANY"]},
+                    "fewshot_examples": {"is_placeholder": False, "value": ()},
+                    "include_meta": {"is_placeholder": False, "value": True},
+                    "prompt_signature_desc": {"is_placeholder": False, "value": None},
+                    "prompt_template": {"is_placeholder": False, "value": None},
+                    "show_progress": {"is_placeholder": False, "value": True},
+                    "task_id": {"is_placeholder": False, "value": "NER"},
+                    "version": Config.get_version(),
+                },
+            ],
+        },
+        "version": Config.get_version(),
+    }
+    Pipeline.deserialize(
+        config=config,
+        tasks_kwargs=[{"engine": {"model": batch_engine.model}, "entities": ["PERSON", "LOCATION", "COMPANY"]}],
+    )
