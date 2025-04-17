7fb21652fe9e89935ac2677ca190d668fb2f0a34
Raphael Mitsch <r.mitsch@outlook.com>
2025-04-17 13:20:58 +0200

    Add ability to generate docs from datasets (#130)
    * Add ability to generate docs from datasets.
    
    * Add generalized column mapping.
    
    * Fix test.
    
    * Expand comments.
    
    ---------
    
    Co-authored-by: Raphael Mitsch <raphael@mantisnlp.com>

--- sieves/data/doc.py
+++ sieves/data/doc.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import dataclasses
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal
 
-from PIL import ImageChops
-from PIL.Image import Image
+from datasets import Dataset
+from PIL import Image, ImageChops
+
+Field = Literal["meta", "results", "uri", "text", "chunks", "id", "images"]
 
 
 @dataclasses.dataclass
@@ -18,14 +20,14 @@ class Doc:
     text: str | None = None
     chunks: list[str] | None = None
     id: str | None = None
-    images: list[Image] | None = None
+    images: list[Image.Image] | None = None
 
     def __post_init__(self) -> None:
         if self.chunks is None and self.text is not None:
             self.chunks = [self.text]
 
     @staticmethod
-    def _are_images_equal(im1: Image, im2: Image) -> bool:
+    def _are_images_equal(im1: Image.Image | None, im2: Image.Image | None) -> bool:
         """Check if two images are equal using PIL Image Channel operations.
         :param im1: First PIL image to compare.
         :param im2: Second PIL image to compare.
@@ -40,8 +42,13 @@ class Doc:
         return ImageChops.difference(im1, im2).getbbox() is None
 
     def __eq__(self, other: object) -> bool:
+        """Compares two `Doc` instances.
+        :return: True if `self` is equal to `other`.
+        :raises: NotImplementedError if `other` isn't of type `Doc`.
+        """
         if not isinstance(other, Doc):
             raise NotImplementedError
+
         # Check if images are equal
         images_equal_check = False
         if self.images is None and other.images is None:
@@ -63,3 +70,27 @@ class Doc:
             and self.results == other.results
             and images_equal_check
         )
+
+    @classmethod
+    def from_hf_dataset(cls, dataset: Dataset, column_map: dict[Field, Any] | None = None) -> list[Doc]:
+        """Generate list of docs from Hugging Face `datasets.Dataset`.
+
+        :param dataset: Dataset to generate `Doc` instances from. If column_map isn't specified to the contrary, dataset
+            must contain at least one column named "text".
+        :param column_map: Which `Doc` attribute to map to which attribute in `dataset`. If None, the mapping "text" ->
+            "text" is assumed.
+        :return: List of `Doc` instances, each representing one row in the dataset.
+        :raises ValueError: If expected columns are not present in the dataset features.
+        """
+        if column_map is None:
+            column_map = {"text": "text"}
+
+        missing_cols = set(column_map.values()) - set(dataset.column_names)
+        if len(missing_cols):
+            raise KeyError(f"Specified columns '{missing_cols}' not found in dataset columns: {dataset.column_names}.")
+
+        docs: list[Doc] = []
+        for row in dataset:
+            docs.append(cls(**{doc_col: row.get(data_col) for doc_col, data_col in column_map.items()}))  # type: ignore[misc]
+
+        return docs
