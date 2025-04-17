128d30ff1bac48f9d221a6f39b70dcc0bf3f9a04
Raphael Mitsch <raphael@mantisnlp.com>
2025-04-17 12:27:52 +0200

    Add ability to generate docs from datasets.

--- sieves/data/doc.py
+++ sieves/data/doc.py
@@ -4,8 +4,8 @@ import dataclasses
 from pathlib import Path
 from typing import Any
 
-from PIL import ImageChops
-from PIL.Image import Image
+from datasets import Dataset
+from PIL import Image, ImageChops
 
 
 @dataclasses.dataclass
@@ -18,14 +18,14 @@ class Doc:
     text: str | None = None
     chunks: list[str] | None = None
     id: str | None = None
-    images: list[Image] | None = None
+    images: list[Image.Image] | None = None
 
     def __post_init__(self) -> None:
         if self.chunks is None and self.text is not None:
             self.chunks = [self.text]
 
     @staticmethod
-    def _are_images_equal(im1: Image, im2: Image) -> bool:
+    def _are_images_equal(im1: Image.Image | None, im2: Image.Image | None) -> bool:
         """Check if two images are equal using PIL Image Channel operations.
         :param im1: First PIL image to compare.
         :param im2: Second PIL image to compare.
@@ -41,7 +41,7 @@ class Doc:
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Doc):
-            raise NotImplementedError
+            return NotImplemented
         # Check if images are equal
         images_equal_check = False
         if self.images is None and other.images is None:
@@ -63,3 +63,27 @@ class Doc:
             and self.results == other.results
             and images_equal_check
         )
+
+    @classmethod
+    def from_hf_dataset(cls, dataset: Dataset, text_column: str = "text") -> list[Doc]:
+        """Generate list of docs from Hugging Face dataset.
+
+        :param dataset: Dataset to generate Docs from. Must have a column with a name that equals `text_column`.
+            Other columns are ignored.
+        :param text_column: Column to pull text from when creating Doc instances.
+        :return: List of Doc instances, each representing one row in the dataset.
+        :raises ValueError: If `text_column` is not present in the dataset features.
+        """
+        if text_column not in dataset.column_names:
+            raise ValueError(
+                f"Specified text_column '{text_column}' not found in dataset columns: {dataset.column_names}."
+            )
+
+        docs: list[Doc] = []
+        for row in dataset:
+            # Assuming row is a dict-like object, typical for Hugging Face datasets.
+            text_content = row.get(text_column)
+            if isinstance(text_content, str):
+                docs.append(cls(text=text_content))
+
+        return docs
