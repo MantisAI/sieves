70b9dd39cfb9d6b8ea4dc069d92fe7a2a03ffe02
Raphael Mitsch <r.mitsch@outlook.com>
2025-04-23 15:45:20 +0200

    Fallbacks for 3rd party engine imports (#137)
    * Add fallback for importing missing 3rd party dependencies. Move engine dependencies (except for Outlines as default) into optional dependency block.
    
    * Add fallback for importing missing 3rd party dependencies. Move engine dependencies (except for Outlines as default) into optional dependency block.
    
    * Attempting to fix vLLM build.
    
    * Attempting to fix vLLM build.
    
    * Remove test.py.
    
    * Add maximize build space action.
    
    * Shift maximize build space action.
    
    * Fix maximization of build space.
    
    * Fix maximization of build space.
    
    * Fix maximization of build space.
    
    * Remove disk space check.
    
    ---------
    
    Co-authored-by: Raphael Mitsch <raphael@mantisnlp.com>

--- sieves/engines/__init__.py
+++ sieves/engines/__init__.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
-from . import dspy_, glix_, huggingface_, instructor_, langchain_, ollama_, outlines_, vllm_
 from .core import EngineInferenceMode, EngineModel, EnginePromptSignature, EngineResult, InternalEngine
-from .dspy_ import DSPy
+from .engine_import import dspy_, glix_, huggingface_, instructor_, langchain_, ollama_, outlines_, vllm_
 from .engine_type import EngineType
 from .wrapper import Engine
 
 __all__ = [
     "dspy_",
-    "DSPy",
     "wrapper",
     "Engine",
     "EngineInferenceMode",
