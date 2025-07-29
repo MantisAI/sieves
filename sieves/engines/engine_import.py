8883a70ca223d342d1ef9e5c2cb4eda1bba4483b
Raphael Mitsch <raphael@climatiq.com>
2025-07-29 13:25:12 +0200

    Fix import shortcut for engines.

--- sieves/engines/engine_import.py
+++ sieves/engines/engine_import.py
@@ -15,67 +15,108 @@ _MISSING_WARNING = (
 
 
 try:
+    from dspy_ import DSPy
+
     from . import dspy_
 except ModuleNotFoundError:
     from . import missing as dspy_
 
+    DSPy = None
     warnings.warn(_MISSING_WARNING.format(missing_dependency="dspy"))
 
 
 try:
+    from glix_ import GliX
+
     from . import glix_
 except ModuleNotFoundError:
     from . import missing as glix_
 
+    GliX = None
     warnings.warn(_MISSING_WARNING.format(missing_dependency="gliner"))
 
 
 try:
+    from huggingface_ import HuggingFace
+
     from . import huggingface_
 except ModuleNotFoundError:
     from . import missing as huggingface_
 
+    HuggingFace = None
     warnings.warn(_MISSING_WARNING.format(missing_dependency="transformers"))
 
 
 try:
+    from instructor_ import Instructor
+
     from . import instructor_
 except ModuleNotFoundError:
     from . import missing as instructor_
 
+    Instructor = None
     warnings.warn(_MISSING_WARNING.format(missing_dependency="instructor"))
 
 
 try:
+    from langchain_ import LangChain
+
     from . import langchain_
 except ModuleNotFoundError:
     from . import missing as langchain_
 
+    LangChain = None
     warnings.warn(_MISSING_WARNING.format(missing_dependency="langchain"))
 
 
 try:
+    from ollama_ import Ollama
+
     from . import ollama_
 except ModuleNotFoundError:
     from . import missing as ollama_
 
+    Ollama = None
     warnings.warn(_MISSING_WARNING.format(missing_dependency="ollama"))
 
 
 try:
+    from outlines_ import Outlines
+
     from . import outlines_
 except ModuleNotFoundError:
     from . import missing as outlines_
 
+    Outlines = None
     warnings.warn(_MISSING_WARNING.format(missing_dependency="outlines"))
 
 
 try:
+    from vllm_ import VLLM
+
     from . import vllm_
 except ModuleNotFoundError:
     from . import missing as vllm_
 
+    VLLM = None
     warnings.warn(_MISSING_WARNING.format(missing_dependency="vllm"))
 
 
-__all__ = ["dspy_", "glix_", "huggingface_", "instructor_", "langchain_", "ollama_", "outlines_", "vllm_"]
+__all__ = [
+    "dspy_",
+    "DSPy",
+    "glix_",
+    "GliX",
+    "huggingface_",
+    "HuggingFace",
+    "instructor_",
+    "Instructor",
+    "langchain_",
+    "LangChain",
+    "ollama_",
+    "Ollama",
+    "outlines_",
+    "Outlines",
+    "vllm_",
+    "VLLM",
+]
