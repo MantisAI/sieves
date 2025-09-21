c0a3ef68e748238769c2ba939c728ca7739cbfbb
Raphael Mitsch <raphael@climatiq.com>
2025-09-21 15:47:08 +0200

    fix: Improve demo.

--- examples/demo.py
+++ examples/demo.py
@@ -1,6 +1,12 @@
-"""Demo for PyData Amsterdam 2025."""
+"""Demo for PyData Amsterdam 2025.
+
+Required additional dependencies:
+- openai
+- outlines
+"""
 
 import os
+from collections import defaultdict
 from pprint import pprint
 from typing import Literal
 
@@ -11,13 +17,6 @@ import pydantic
 
 from sieves import Doc, Engine, Pipeline, tasks
 
-docs = [
-    Doc(
-        uri="https://www.euronews.com/my-europe/2025/09/11/fact-check-is-the-eu-about-to-start-scanning-your-text-messages"
-    )
-]
-engine = Engine(model=outlines.from_openai(openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"]), model_name="gpt-5"))
-
 
 class Country(pydantic.BaseModel, frozen=True):
     """Information to look for in document."""
@@ -27,21 +26,29 @@ class Country(pydantic.BaseModel, frozen=True):
     stance_on_chat_message_scanning_proposal: Literal["pro", "undecided", "contra", "unknown"]
 
 
-pipe = Pipeline(
-    [
-        # Add document ingestion.
-        tasks.Ingestion(export_format="markdown"),
-        # Chunk just in case text is too long to fit into model context window.
-        tasks.Chunking(chonkie.TokenChunker()),
-        # Define information extraction task.
-        tasks.InformationExtraction(entity_type=Country, engine=engine),
+if __name__ == '__main__':
+    docs = [
+        Doc(
+            uri="https://www.techradar.com/computing/cyber-security/chat-control-the-list-of-countries-opposing-the-law-grows-but-support-remains-strong"
+        )
     ]
-)
+    engine = Engine(model=outlines.from_openai(openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"]), model_name="gpt-5"))
 
-for doc in pipe(docs):
-    results = (
-        (res.name, res.stance_on_chat_message_scanning_proposal)
-        for res in doc.results["InformationExtraction"]
-        if res.in_eu
+    pipe = Pipeline(
+        [
+            # Add document ingestion.
+            tasks.Ingestion(export_format="markdown"),
+            # Chunk just in case text is too long to fit into model context window.
+            tasks.Chunking(chonkie.TokenChunker()),
+            # Define information extraction task.
+            tasks.InformationExtraction(entity_type=Country, engine=engine),
+        ]
     )
-    pprint(sorted(results, key=lambda item: item[1]))
+
+    for doc in pipe(docs):
+        results = defaultdict(list)
+        for res in doc.results["InformationExtraction"]:
+            if res.in_eu:
+                results[res.stance_on_chat_message_scanning_proposal].append(res.name)
+
+        pprint(results)
