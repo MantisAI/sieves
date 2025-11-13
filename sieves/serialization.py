a4537f362ed4040fe55894b60a973b1573cc51b3
Raphael Mitsch <raphael@climatiq.com>
2025-11-13 16:11:17 +0100

    chore: Bump version.

--- sieves/serialization.py
+++ sieves/serialization.py
@@ -79,7 +79,7 @@ class Config(pydantic.BaseModel):
 
         :return str: Version string from setup.cfg metadata.
         """
-        return "0.19.1"
+        return "0.19.2"
 
     version: str = get_version()
     cls_name: str
