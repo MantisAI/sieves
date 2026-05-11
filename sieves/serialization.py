d554aaebc1e5a48b73851742f8b7bea4743639e2
Raphael Mitsch <raphael@climatiq.com>
2026-05-11 16:17:32 +0200

    chore: Bump pins.

--- sieves/serialization.py
+++ sieves/serialization.py
@@ -79,7 +79,7 @@ class Config(pydantic.BaseModel):
 
         :return str: Version string from setup.cfg metadata.
         """
-        return "1.0.3"
+        return "1.0.4"
 
     version: str = get_version()
     cls_name: str
